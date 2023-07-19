1e92b0b7d30d653362fc57fe6444158bba270f16
adeprastyo <adeprastyo101@gmail.com>
2023-07-19 22:56:45 +0700

    second commit

--- src/components/CustNavbar.jsx
+++ src/components/CustNavbar.jsx
@@ -1,25 +1,53 @@
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 export default function CustNavbar() {
   return (
-    <Navbar rounded className="shadow-lg px-16 py-10">
-      <Navbar.Brand href="https://flowbite-react.com">
-        <span className="self-center whitespace-nowrap text-3xl font-bold dark:text-white">
-          <Link to="/">
-            CHALLENGE PT <span className="text-orange-500">NU</span>
-            <span className="text-blue-900">TECH</span>
-          </Link>
-        </span>
-      </Navbar.Brand>
+    <>
+      <div className="w-full shadow-md">
+        <nav className="container mx-auto flex items-center justify-between p-10 ">
+          <div className="flex items-center flex-shrink-0 mr-6">
+            <Link to="/">
+              <span className="self-center whitespace-nowrap text-3xl font-bold dark:text-white">
+                CHALLENGE PT <span className="text-orange-500">NU</span>
+                <span className="text-blue-900">TECH</span>
+              </span>
+            </Link>
+          </div>
 
-      <Navbar.Collapse>
-        <Navbar.Link active href="#" className="text-lg">
-          <Link to="/data">
-            <p>Manage Data</p>
-          </Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+          <div className="flex">
+            <div className="text-lg lg:flex-grow">
+              <Link
+                to="/data"
+                className="block mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-orange-500 mr-4"
+              >
+                Manage Data
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
   );
+}
+
+{
+  /* // return (
+  //   <Navbar rounded className="shadow-lg px-16 py-10">
+  //     <Link to="/">
+  //       <Navbar.Brand>
+  //         <span className="self-center whitespace-nowrap text-3xl font-bold dark:text-white">
+  //           CHALLENGE PT <span className="text-orange-500">NU</span>
+  //           <span className="text-blue-900">TECH</span>
+  //         </span>
+  //       </Navbar.Brand>
+  //     </Link>
+  //     <Navbar.Collapse>
+  //       <Link to="/data">
+  //         <Navbar.Brand className="text-lg">
+  //           <p>Manage Data</p>
+  //         </Navbar.Brand>
+  //       </Link>
+  //     </Navbar.Collapse>
+  //   </Navbar>
+  // ); */
 }
